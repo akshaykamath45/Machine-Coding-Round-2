2b72188ecb5ac1c8c07bff0db8eba0bd3cbc8576
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-06-18 21:27:27 +0530

    updated code

--- habit-tracker/src/App.jsx
+++ habit-tracker/src/App.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 import { Routes, Route, NavLink } from "react-router-dom";
 import { ArchivedHabits } from "./pages/ArchivedHabits";
-import {LandingPage} from "./pages/LandingPage";
+import { LandingPage } from "./pages/LandingPage";
 const App = () => {
   return (
     <div className="app">
-
       <nav>
-        <NavLink to="/">Home</NavLink> || 
+        <NavLink to="/">Home</NavLink> ||
         <NavLink to="/archived">Archived</NavLink>
       </nav>
       <Routes>
-        <Route path="/" element={<LandingPage/>}></Route>
+        <Route path="/" element={<LandingPage />}></Route>
         <Route path="/archived" element={<ArchivedHabits />}></Route>
       </Routes>
     </div>
--- habit-tracker/src/pages/ArchivedHabits.jsx
+++ habit-tracker/src/pages/ArchivedHabits.jsx
@@ -7,5 +7,3 @@ export const ArchivedHabits = () => {
     </div>
   )
 }
-
-
